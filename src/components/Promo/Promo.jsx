be02231863175a8b104cc6bba3e52ec28fcdc4c6
Andrew <63206688+AndrewGonnaCode@users.noreply.github.com>
2022-02-19 17:41:14 +0300

    form

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -9,6 +9,14 @@ const Promo = () => {
     <section className="promo">
       <div className="promo-bg">
         <img src={promoLogo} alt="" className="promo-logo" />
+        <div className="promo-info">
+          <p className="promo-info__price text-shadow">Mint price @ 0.1 ETH</p>
+          <div className="promo-info__status text-shadow">
+            <p className="text-shadow">Public sale is now Live.</p>
+            <p className="text-shadow">You can proceed to minting.</p>
+          </div>
+          <button className="btn">CONNECT WALLET</button>
+        </div>
       </div>
     </section>
   );
