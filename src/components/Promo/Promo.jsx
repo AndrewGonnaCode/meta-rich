2f3d85fbdc41080e06f17adacf556541f54e267c
Andrew <63206688+AndrewGonnaCode@users.noreply.github.com>
2022-02-19 13:59:23 +0300

    logo

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import "./promo.scss";
 import banner from "../../assets/images/kong.png";
 import bg from "../../assets/images/new/bg.jpeg";
+import promoLogo from "../../assets/images/new/logo.png";
 
 const Promo = () => {
   return (
     <section className="promo">
-      <div className="promo__bg">
-        <img src={bg} alt="banner" />
+      <div className="promo-bg">
+        <img src={promoLogo} alt="" className="promo-logo" />
       </div>
     </section>
   );
